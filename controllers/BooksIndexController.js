8a694d6941bc2a7e8a845eb910ca754d0b110fcf
Cory Fauver <cofauver@gmail.com>
2016-04-18 11:31:27 -0700

    adding all the viewing info on index

--- controllers/BooksIndexController.js
+++ controllers/BooksIndexController.js
@@ -6,9 +6,10 @@ function BooksIndexController( $http ) {
   var vm = this;
   $http({
     method: 'GET',
-    url: 'https://super-crud.herokuapp.com/books/ajopoadfpakpokpok'
+    url: 'https://super-crud.herokuapp.com/books'
   }).then(function(response){
     console.log(response.data);
+    vm.books = response.data.books;
   }).then(function(error){
     console.log(error);
   });
